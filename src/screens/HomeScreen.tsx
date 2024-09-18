1cfe2f0c30bab93c71a74e421f2b60c9aa08faa5
Nathlo <thyalan.aurlence@gmail.com>
2024-09-18 19:46:31 -0400

    Zustand store feature

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useStore } from '../store/Store'
 
 const HomeScreen = () => {
+  const CoffeeList = useStore( (state: any) => state.CoffeeList );
+  const BeanList = useStore( (state: any) => state.BeanList );
+  // console.log('CoffeeLIST =', CoffeeList.length)
+
   return (
     <View>
       <Text>HomeScreen</Text>
